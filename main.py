463f554a692c8c0f2bd98ce06798d59a64c5ca7b
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-04-21 10:16:05 -0400

    fixing comment

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def on_message(message):
         with open("count_time_delay.json", "w") as file:
             json.dump(value, f)
     
-    # code to count with Baby Mario
+    # code to count with Baby Luigi
     if message.author.id == 647946543411757059 or message.author.id == 1067461387242192896:
         with open("count_time_delay.json", "r") as file:
             time = json.load(file)
