1b7fbb0bc3edae5b1c2ec88cb7e54b063a82a23e
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-04-21 06:30:47 -0400

    Implement basic Discord bot functionality
    Added a Discord bot that responds to specific users' messages by incrementing a number sent in the message.

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import discord
+import asyncio
+import os
 
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True  # needed to read messages
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+    # ignore your own bot
+    if message.author == client.user:
+        return
+
+    # simple test command
+    if message.author.id == 647946543411757059 or message.author.id == 1067461387242192896:
+        await asyncio.sleep(2)
+        num = int(message.content)
+        num += 1
+        num = str(num)
+        await message.channel.send(num)
+
+    
+
+client.run(TOKEN)
