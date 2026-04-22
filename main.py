180ef006b1135f89b19caebf408e59588a62af55
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-04-22 05:27:11 -0400

    Fix indentation and return statements in main.py

--- main.py
+++ main.py
@@ -27,9 +27,10 @@ async def on_message(message):
         with open("count_time_delay.json", "w") as file:
             json.dump(value, file)
         await message.channel.send(":thumbsup:")
+        return
     
     # code to count with Baby Luigi
-    elif message.author.id == 647946543411757059 or message.author.id == 1067461387242192896:
+    if message.author.id == 647946543411757059 or message.author.id == 1067461387242192896:
         if message.channel.id == 1496153092000059392:
             with open("count_time_delay.json", "r") as file:
                 time = json.load(file)
@@ -38,6 +39,7 @@ async def on_message(message):
             num += 1
             num = str(num)
             await message.channel.send(num)
+            return
 
     
 
